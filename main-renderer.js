4239ac6855e2da5647d5d635c3f5394a2432689c
Melodie X <mxiong@uwaterloo.ca>
2025-05-23 23:44:49 -0400

    Delete main-renderer.js

--- main-renderer.js
+++
@@ -1,7 +0,0 @@
-console.log("mainrenderer.js loaded!");
-
-//open the other window
-const openPopup = document.getElementById('open-popup');
-openPopup.addEventListener('click', () =>{
-  window.electronAPI.openPopup();
-})
