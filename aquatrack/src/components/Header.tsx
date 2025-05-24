5633e569396aae74fbb7f4458520da29317aa7d1
Melodie X <mxiong@uwaterloo.ca>
2025-05-24 19:29:00 -0400

    committing all default files

---
+++ aquatrack/src/components/Header.tsx
@@ -0,0 +1,17 @@
+function Header(){
+  return(
+    <div>
+      <div>
+        AquaTrack
+      </div>
+      <div>
+        <button>Home</button>
+        <button>Graphs</button>
+        <button>About</button>
+        <input type="text" placeholder='login'/>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
